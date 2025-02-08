730e779cfcfbd35d415dbd6ca4bda58261c17c2a
Shaamad Allison <167145698+caol777@users.noreply.github.com>
2025-02-07 19:21:22 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
 
-# Check if the script is run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
+# Back ups important directories into /root
+# Make sure zip is installed
 
-# Variables
-BACKUP_SOURCE="/etc /home /var/www /bin /usr/bin"
-BACKUP_DEST="/backup"
-BACKUP_NAME="backup_$(date +%Y%m%d%H%M%S).tar.gz"
-REMOTE_SERVER="user@remote.server:/path/to/backup"
-LOG_FILE="/var/log/backup.log"
-
-# Create backup destination directory if it doesn't exist
-mkdir -p $BACKUP_DEST
-
-# Create the backup
-tar -czf $BACKUP_DEST/$BACKUP_NAME $BACKUP_SOURCE
-
-# Check if the backup was successful
-if [ $? -eq 0 ]; then
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - Backup created successfully: $BACKUP_DEST/$BACKUP_NAME" >> $LOG_FILE
-else
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - Backup failed" >> $LOG_FILE
+if [ $(whoami) != "root" ]; then
+    echo "Script must be run as root"
     exit 1
 fi
 
-# Optionally, upload the backup to a remote server
-scp $BACKUP_DEST/$BACKUP_NAME $REMOTE_SERVER
+# Define the directories to backup
+backup_directories="/etc /home /bin /var/www /usr/bin"
 
-# Check if the upload was successful
-if [ $? -eq 0 ]; then
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - Backup uploaded successfully to $REMOTE_SERVER" >> $LOG_FILE
-else
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - Backup upload failed" >> $LOG_FILE
-    exit 1
-fi
+# Zip the specified directories into the backup directory
+zip -r "/root/backup.zip" $backup_directories
 
-echo "Backup process completed successfully."
+# Rename the backup file to the specified name
+
+mv "/root/backup.zip" "/root/.../GF2451fasfNGJDNFFI!#T!%)JDWF"
