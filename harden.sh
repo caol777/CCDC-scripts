7b3f3f754767cd5980c39f9bebe2b2ef8cc729ef
Shaamad Allison <167145698+caol777@users.noreply.github.com>
2025-05-26 00:59:38 -0400

    Update harden.sh

--- harden.sh
+++ harden.sh
@@ -24,6 +24,9 @@ net.ipv4.tcp_rfc1337 = 1
 net.ipv4.icmp_ignore_bogus_error_responses = 1
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.all.log_martians = 1
 net.ipv4.conf.default.accept_redirects = 0
