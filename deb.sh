54c791a70546abafdf2e577486e6e5508b93fe65
Shaamad Allison <167145698+caol777@users.noreply.github.com>
2025-10-11 02:26:18 -0400

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -58,7 +58,7 @@ update_system
 
 # Install packages
 # Added iptables-persistent and iptables-services to the package list
-packageList=" epel-release inotify-tools rsyslog git fail2ban zip tmux net-tools htop tcpdump e2fsprogs ufw rkhunter whowatch curl debsums chkrootkit iptables-persistent iptables-services clamav clamav-daemon"
+packageList=" epel-release inotify-tools rsyslog git fail2ban zip tmux net-tools htop tcpdump e2fsprogs nmap ufw rkhunter whowatch curl debsums chkrootkit iptables-persistent iptables-services clamav clamav-daemon"
 install_package $packageList
 
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
