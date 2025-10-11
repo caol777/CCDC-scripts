21c456bd3c3625fcff029c0eb77508ac7b278c35
Shaamad Allison <167145698+caol777@users.noreply.github.com>
2025-10-11 01:40:46 -0400

    Create alma.sh

---
+++ alma.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+
+passwd -l root
+
+# Determine the package manager
+if command -v apt > /dev/null; then
+    pkgManager="apt"
+elif command -v dnf > /dev/null; then
+    pkgManager="dnf"
+elif command -v yum > /dev/null; then
+    pkgManager="yum"
+else
+    echo "No known package manager found. Script will exit."
+    exit 1
+fi
+
+echo "Using package manager: $pkgManager"
+
+# Define a generic update, install, and remove function
+update_system() {
+    sudo $pkgManager update -y
+    if [ "$pkgManager" = "apt" ]; then
+        sudo $pkgManager upgrade -y
+    fi
+}
+
+install_package() {
+    for pkg in "$@"; do
+        echo "Attempting to install $pkg..."
+        sudo $pkgManager install -y "$pkg" || echo "Failed to install $pkg. Skipping..."
+    done
+}
+
+remove_package() {
+    if [ "$pkgManager" = "apt" ]; then
+        sudo $pkgManager purge -y "$@"
+    else
+        sudo $pkgManager remove -y "$@"
+    fi
+}
+
+fix_missing_apt() {
+    if [ "$pkgManager" = "apt" ]; then
+        sudo apt update --fix-missing
+    fi
+}
+
+enable_service() {
+    sudo systemctl enable "$@"
+    sudo systemctl start "$@"
+}
+
+fix_missing_apt
+
+# Update system
+update_system
+
+# Install packages
+# Added iptables-persistent and iptables-services to the package list
+packageList="inotify-tools rsyslog git fail2ban zip tmux net-tools htop e2fsprogs ufw rkhunter whowatch curl debsums chkrootkit iptables-persistent iptables-services clamav clamav-daemon"
+install_package $packageList
+
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
+chmod +x pspy64
+
+remove_package cron
+
+# Special handling for UFW, considering its availability
+# Removed the UFW installation from here because it's now included directly in the package list
+
+# Enable and start fail2ban
+enable_service fail2ban
+enable_service clamav-freshclam
+echo "Package installation and configuration completed."
+
+
+
+
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "Protocol 2" >> /etc/ssh/sshd_config
+#SSH whitelist
+echo "AllowUsers jmomey plinktern" >> /etc/ssh/sshd_config
+
+systemctl restart sshd
+
+apt install ufw -y
+#metasploit default port
+ufw deny 4444
+
+ufw default deny incoming
+ufw default allow outgoing
+#sets firewall rules
+ufw allow OpenSSH
+ufw allow Apache Full
+ufw allow 80 tcp 
+ufw enable
+
+chmod 644 /etc/passwd
+
+pwck
+
+chattr +i /etc/bind
+chattr +i /etc/ssh/sshd_config
