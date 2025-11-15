43b95ac92d9fc3f18c43753bec224fae9e9d3b1f
Shaamad Allison <167145698+caol777@users.noreply.github.com>
2025-11-15 11:46:18 -0500

    Delete windows/UserAuditing.ps1

--- windows/UserAuditing.ps1
+++
@@ -1,9 +0,0 @@
-#predefined list (change for each comp)
-$acclist = @("jmoney", "plinktern") | ForEach-Object { $_.Trim().ToLower() }
-$acc = Get-LocalUser | Select-Object -ExpandProperty Name | ForEach-Object { $_.Trim().ToLower() }
-
-foreach ($user in $acc) {
-    if ($user -notin $acclist) {
-        net user $user /active:no
-    } 
-}
