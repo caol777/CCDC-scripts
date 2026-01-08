3759fdd02e3d9f3f7a376ba52a35bdcab6b5de15
Shaamad Allison <167145698+caol777@users.noreply.github.com>
2026-01-07 23:43:25 -0500

    Update env.sh

--- Linux/env.sh
+++ Linux/env.sh
@@ -1,3 +1,7 @@
 export DISPATCHER="192.168.1.50"      # Your management/Blue Team IP
 export LOCALNETWORK="192.168.1.0/24"   # Your internal team subnet
 export CCSHOST="192.168.220.70"       # The IP of the scoring engine/NAT router
+
+
+# Temporarily allow outgoing web traffic to download tools
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
